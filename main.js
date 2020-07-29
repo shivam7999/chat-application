936ff244677d9aea64ec006ae166987b61798d7c
shivam7999 <18ucc167@lnmiit.ac.in>
2020-07-30 00:32:08 +0530

    First commit

---
+++ main.js
@@ -0,0 +1,22 @@
+var express=require('express');
+var socket=require('socket.io');
+//app setup
+var app=express();
+var server=app.listen(8000,function(){
+console.log('listening to request on port 8000')
+
+});
+//static files
+//app.use(express.static('public'));
+//socket setup
+var io = socket(server);
+io.on('connection', function(socket){
+console.log('made socket connection',socket.id);
+
+socket.on('chat',function(data){
+io.sockets.emit('chat',data);
+});
+socket.on('typing',function(data){
+socket.broadcast.emit('typing',data);
+});
+});
